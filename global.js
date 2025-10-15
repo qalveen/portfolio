0ca953d56a4642c94a87d3951937887a6ce3a52d
qalveen <cmagsie@ucsd.edu>
2025-10-15 16:30:56 -0700

    c

--- global.js
+++ global.js
@@ -19,7 +19,7 @@ document.body.prepend(nav);
 const BASE_PATH =
   location.hostname === 'localhost' || location.hostname === '127.0.0.1'
     ? '/'
-    : '/portofolio/'; // <-- change if your repo name is different
+    : '/portfolio/'; // 
 
 for (let p of pages) {
   let url = p.url;
