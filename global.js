07b83b77367ce9aadc094875316969b7e4bf16ac
qalveen <cmagsie@ucsd.edu>
2025-10-15 16:13:24 -0700

    nav

--- global.js
+++ global.js
@@ -4,10 +4,40 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let navLinks = $$("nav a");
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/qalveen', title: 'GitHub' }
+];
 
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
+let nav = document.createElement('nav');
+document.body.prepend(nav);
 
-currentLink?.classList.add("current");
+const BASE_PATH =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? '/'
+    : '/portofolio/'; // change this to match your repo name if different
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  a.toggleAttribute('target', a.host !== location.host);
+
+  nav.append(a);
+}
