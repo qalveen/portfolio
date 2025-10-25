4f2e1f025f93766ce7feaa91add2053b5dcdf840
qalveen <cmagsie@ucsd.edu>
2025-10-24 18:52:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -19,3 +19,16 @@ renderProjects(latestProjects, projectsContainer, 'h2');
 // ─────────────────────────────────────────────
 const githubData = await fetchGitHubData('qalveen');
 console.log(githubData);
+
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+  profileStats.innerHTML = `
+    <dl>
+      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+      <dt>Followers:</dt><dd>${githubData.followers}</dd>
+      <dt>Following:</dt><dd>${githubData.following}</dd>
+    </dl>
+  `;
+}
